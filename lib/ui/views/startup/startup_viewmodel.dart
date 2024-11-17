5e76a069da4e37b72608c4723d8082272a6f997e
Alvaro Lopez Garcia <alopegarci@gmail.com>
2024-11-17 09:41:19 +0100

    change icon and add update with google play

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,18 +1,38 @@
+import 'package:cv_responsive/app/app.bottomsheets.dart';
 import 'package:stacked/stacked.dart';
 import 'package:cv_responsive/app/app.locator.dart';
 import 'package:cv_responsive/app/app.router.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:in_app_update/in_app_update.dart';
 
 class StartupViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
+  final _bottomSheetService = locator<BottomSheetService>();
+
+  AppUpdateInfo? _updateInfo;
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await checkForUpdate();
+    if (_updateInfo?.updateAvailability == UpdateAvailability.updateAvailable) {
+      InAppUpdate.performImmediateUpdate();
+    }
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
 
     _navigationService.replaceWithHomeView();
+  }
+
+  Future<void> checkForUpdate() async {
+    InAppUpdate.checkForUpdate().then((info) {
+      _updateInfo = info;
+    }).catchError((e) {
+      _bottomSheetService.showCustomSheet(
+        variant: BottomSheetType.notice,
+        title: "Error", // ALG: añadir traducciones
+        description: e.toString(),
+      );
+    });
   }
 }
